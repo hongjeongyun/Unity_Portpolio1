200e4590a92ea992e92efbd79a0cd0bb65bae3b0
hermes <dreamcastrar@gmail.com>
2024-12-16 19:30:34 +0900

    Create LoginLink.cs
    로그인링크에 대한 마우스 상호작용기능

---
+++ Assets/Script/LoginLink.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class LoginLink : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{    
+    [SerializeField] TMP_Text Login_link;
+    public void OnPointerClick(PointerEventData eventData) => Application.OpenURL(Login_link.text);
+    public void OnPointerEnter(PointerEventData eventData) => Login_link.color = Color.green;    
+    public void OnPointerExit(PointerEventData eventData) => Login_link.color = Color.white;
+}
