d008509d8a4514ba2663967119096d5e21db6825
hermes <dreamcastrar@gmail.com>
2024-12-17 19:33:38 +0900

    Minor Update

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Main : MonoBehaviour
+{    
+    [SerializeField] Button Button_Get;
+    [SerializeField] TMP_InputField UriInputField;
+    [SerializeField] TMP_Text Login_link;
+    [SerializeField] TMP_Text Result;
+     
+    void Start()
+    {        
+        Login_link.text = PSNIDBASE64.LOGIN_URL;
+        
+        Button_Get.onClick.AddListener(() => 
+        {
+            if (UriInputField.text == string.Empty || UriInputField.text == null)
+                return;
+            
+            var result = PSNIDBASE64.GetBase64ID(UriInputField.text);                        
+            Result.text = result;
+            Result.fontSize = 50;                                                                        
+        });
+    }    
+}
